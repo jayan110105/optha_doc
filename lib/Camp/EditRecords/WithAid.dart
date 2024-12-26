5674bea84386add29ab4da6f422ce346d137a24d
jayan110105 <jayan.mitmpl2022@learner.manipal.edu>
2024-12-26 16:56:46 +0530

    Made changes to EditWithAid

--- lib/Camp/EditRecords/WithAid.dart
+++ lib/Camp/EditRecords/WithAid.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class EditWithAidCard extends StatelessWidget {
+class EditWithAidCard extends StatefulWidget {
   final Map<String, dynamic>? selectedRecord;
 
   const EditWithAidCard({super.key, required this.selectedRecord});
 
-  Widget _buildDetailRow(String label, String value) {
-    // Determine if the label requires a larger font size
+  @override
+  State<EditWithAidCard> createState() => _WithAidCardState();
+}
+
+class _WithAidCardState extends State<EditWithAidCard> {
+
+  Widget _buildDetailRow(String label, String value, Function(String)? onEdit) {
     bool isHighlighted = label == "Distance Vision" || label == "Near Vision";
 
     return Column(
@@ -22,15 +27,16 @@ class EditWithAidCard extends StatelessWidget {
         ),
         const SizedBox(height: 4),
         Flexible(
-          child: Text(
-            value,
+          child: TextField(
+            controller: TextEditingController(text: value),
+            onChanged: onEdit,
             style: TextStyle(
-              fontSize: isHighlighted ? 18 : 14, // Larger font size for highlighted labels
+              fontSize: isHighlighted ? 18 : 14,
               fontWeight: FontWeight.bold,
               color: Color(0xFF163351),
             ),
           ),
-        ),
+        )
       ],
     );
   }
@@ -66,20 +72,56 @@ class EditWithAidCard extends StatelessWidget {
           ),
           child: GridView(
             shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(), // Prevents scrolling inside the grid
+            physics: const NeverScrollableScrollPhysics(),
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // Number of columns
-              mainAxisSpacing: 12, // Spacing between rows
-              crossAxisSpacing: 8, // Spacing between columns
-              childAspectRatio: 3, // Adjust to control the height/width ratio
+              crossAxisCount: 2,
+              mainAxisSpacing: 12,
+              crossAxisSpacing: 8,
+              childAspectRatio: 3,
             ),
             children: [
-              _buildDetailRow("Distance Vision", record['withAid'][eye]['distanceVision']),
-              _buildDetailRow("Near Vision", record['withAid'][eye]['nearVision']),
-              _buildDetailRow("D.V. Sphere", record['withAid'][eye]['dvSphere']),
-              _buildDetailRow("N.V. Sphere", record['withAid'][eye]['nvSphere']),
-              _buildDetailRow("Cylinder", record['withAid'][eye]['cyl']),
-              _buildDetailRow("Axis", "${record['withAid'][eye]['axis']}°"),
+              _buildDetailRow(
+                "Distance Vision",
+                record['withAid'][eye]['distanceVision'],
+                    (value) => setState(() {
+                  record['withAid'][eye]['distanceVision'] = value;
+                }),
+              ),
+              _buildDetailRow(
+                "Near Vision",
+                record['withAid'][eye]['nearVision'],
+                    (value) => setState(() {
+                  record['withAid'][eye]['nearVision'] = value;
+                }),
+              ),
+              _buildDetailRow(
+                "D.V. Sphere",
+                record['withAid'][eye]['dvSphere'],
+                    (value) => setState(() {
+                  record['withAid'][eye]['dvSphere'] = value;
+                }),
+              ),
+              _buildDetailRow(
+                "N.V. Sphere",
+                record['withAid'][eye]['nvSphere'],
+                    (value) => setState(() {
+                  record['withAid'][eye]['nvSphere'] = value;
+                }),
+              ),
+              _buildDetailRow(
+                "Cylinder",
+                record['withAid'][eye]['cyl'],
+                    (value) => setState(() {
+                  record['withAid'][eye]['cyl'] = value;
+                }),
+              ),
+              _buildDetailRow(
+                "Axis",
+                "${record['withAid'][eye]['axis']}°",
+                    (value) => setState(() {
+                  record['withAid'][eye]['axis'] = value.replaceAll("°", "");
+                }),
+              ),
             ],
           ),
         ),
@@ -95,29 +137,34 @@ class EditWithAidCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
-            children: const [
-              Icon(
-                Icons.visibility,
-                color: Color(0xFF163351),
-                size: 24,
-              ),
-              SizedBox(width: 8),
-              Text(
-                "With Aid",
-                style: TextStyle(
-                  color: Color(0xFF163351),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: const [
+                  Icon(
+                    Icons.visibility,
+                    color: Color(0xFF163351),
+                    size: 24,
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    "With Aid",
+                    style: TextStyle(
+                      color: Color(0xFF163351),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
           const SizedBox(height: 24),
           Column(
             children: [
-              _buildEyeDetails("left", selectedRecord!),
+              _buildEyeDetails("left", widget.selectedRecord!),
               const SizedBox(height: 24),
-              _buildEyeDetails("right", selectedRecord!),
+              _buildEyeDetails("right", widget.selectedRecord!),
             ],
           ),
           const SizedBox(height: 24),
@@ -137,14 +184,22 @@ class EditWithAidCard extends StatelessWidget {
                     size: 20,
                   ),
                   const SizedBox(width: 8),
-                  Text(
-                    "I.P.D: ${selectedRecord?['withAid']['ipd']} mm",
-                    style: const TextStyle(
-                      color: Color(0xFF163351),
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+                  SizedBox(
+                    width: 50,
+                    child: TextField(
+                      controller: TextEditingController(
+                        text: widget.selectedRecord?['withAid']['ipd'],
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          widget.selectedRecord?['withAid']['ipd'] = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
