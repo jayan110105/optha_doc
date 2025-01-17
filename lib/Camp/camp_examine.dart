d91d56703d66711587d88eb68fbf73e156004b66
jayan110105 <jayan.mitmpl2022@learner.manipal.edu>
2025-01-17 17:24:28 +0530

    Comorbid and CustomCheckbox

--- lib/Camp/camp_examine.dart
+++ lib/Camp/camp_examine.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:opthadoc/Components/CardComponent.dart';
 import 'package:opthadoc/Components/CustomRadioGroup.dart';
+import 'package:opthadoc/Components/CustomTextArea.dart';
 import 'package:opthadoc/Components/InputField.dart';
 import 'package:opthadoc/Components/Label.dart';
 import 'package:opthadoc/Components/ProgressStep.dart';
+import 'package:opthadoc/Components/CustomCheckbox.dart';
 
 class CampExamine extends StatefulWidget {
   const CampExamine({super.key});
@@ -28,9 +30,22 @@ class _CampExamineState extends State<CampExamine> {
     {"label": "History of glasses"},
   ];
 
+  final Map<String, bool> comorbidities = {
+    "Diabetes mellitus": false,
+    "Hypertension": false,
+    "Heart disease": false,
+    "Asthma": false,
+    "Allergy to dust/meds": false,
+    "Benign prostatic hyperplasia on treatment": false,
+    "Is the patient On Antiplatelets" : false,
+  };
+
+  final Map<String, TextEditingController> controllers = {};
+
   final List<Map<String, dynamic>> steps = [
     {"id": "patient", "title": "Patient", "icon": Icons.person},
     {"id": "history", "title": "History", "icon": Icons.description},
+    {"id": "comorbid", "title": "Comorbid", "icon": "assets/icons/vital_signs.svg"},
     {"id": "exam", "title": "Exam", "icon": "assets/icons/stethoscope.svg"},
     {"id": "workup", "title": "Workup", "icon": Icons.biotech},
     {"id": "dilated", "title": "Dilated", "icon": Icons.visibility},
@@ -61,7 +76,26 @@ class _CampExamineState extends State<CampExamine> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    // Initialize controllers for each comorbidity
+    for (var key in comorbidities.keys) {
+      controllers[key] = TextEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    // Dispose controllers to avoid memory leaks
+    for (var controller in controllers.values) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final List<String> comorbidityKeys = comorbidities.keys.toList();
 
     final stepWidgets = [
       Column(
@@ -141,6 +175,94 @@ class _CampExamineState extends State<CampExamine> {
               ],
             );
           }),
+          selectedValues['History of glasses'] == 'yes'
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Label(text: "Are PG comfortable ?"),
+              CustomRadioGroup(
+                selectedValue: selectedValues["Are PG comfortable ?"],
+                onChanged: (value) {
+                  setState(() {
+                    selectedValues["Are PG comfortable ?"] = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 16),
+              Label(text: "Last Glass change"),
+              InputField(hintText: "Enter date or duration"),
+            ],
+          )
+              : SizedBox.shrink(),
+          const SizedBox(height: 16),
+          Label(text: "Previous surgery/laser rx"),
+          CustomRadioGroup(
+            selectedValue: selectedValues["Previous surgery/laser rx"],
+            onChanged: (value) {
+              setState(() {
+                selectedValues["Previous surgery/laser rx"] = value;
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+          selectedValues['Previous surgery/laser rx'] == 'yes'
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Label(text: "Details of surgery/procedure"),
+              CustomTextArea(hintText: "Enter surgery details", minLines: 3,),
+            ],
+          )
+              : SizedBox.shrink(),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Comorbidities and Medications",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF163351),
+            ),
+          ),
+          SizedBox(height: 24),
+          Column(
+            children: [
+              for (int i = 0; i < comorbidityKeys.length; i += 2)
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomCheckbox(
+                        value: comorbidities[comorbidityKeys[i]] ?? false,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            comorbidities[comorbidityKeys[i]] = value ?? false;
+                          });
+                        },
+                        controllers: controllers,
+                        text: comorbidityKeys[i],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    if (i + 1 < comorbidityKeys.length) // Check if next item exists
+                      Expanded(
+                        child: CustomCheckbox(
+                          value: comorbidities[comorbidityKeys[i + 1]] ?? false,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              comorbidities[comorbidityKeys[i + 1]] = value ?? false;
+                            });
+                          },
+                          controllers: controllers,
+                          text: comorbidityKeys[i + 1],
+                        ),
+                      ),
+                  ],
+                ),
+            ],
+          ),
         ],
       ),
     ];
---
+++ lib/Components/CustomCheckbox.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:opthadoc/Components/Label.dart';
+
+class CustomCheckbox extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool?> onChanged;
+  final Map<String, TextEditingController> controllers;
+  final String text; // New required parameter
+
+  const CustomCheckbox({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    required this.controllers,
+    required this.text, // Add this parameter to the constructor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // crossAxisAlignment: CrossAxisAlignment.start, // Align items along the center baseline
+      children: [
+        Checkbox(
+          visualDensity: VisualDensity.compact,
+          value: value,
+          onChanged: (bool? newValue) {
+            onChanged(newValue);
+          },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          activeColor: const Color(0xFF163351),
+          checkColor: Colors.white,
+          side: const BorderSide(
+            color: Color(0xFFD3D3D3),
+            width: 2,
+          ),
+        ),
+        Flexible(
+          child: Label(text: text),
+        ),
+      ],
+    );
+  }
+}
